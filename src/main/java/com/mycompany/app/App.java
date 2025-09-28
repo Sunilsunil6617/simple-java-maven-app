0a57d4254e27e60267fb6f06ab8304b1471b2a02
Sunilsunil6617 <105533521+Sunilsunil6617@users.noreply.github.com>
2025-09-28 14:30:50 +0530

    first commit in dev

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,12 +3,12 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
-public class App
-{
+public class App {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World! from dev";
 
-    public App() {}
+    public App() {
+    }
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
